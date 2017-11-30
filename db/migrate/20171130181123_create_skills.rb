ad9768429f714090c152df9f1bdf1d6bc27e3c4a
Casey Lund <caseylund@caseys-air.ad.uvu.edu>
2017-11-30 15:33:35 -0700

    added portfolio items via Resource manager

--- db/migrate/20171130181123_create_skills.rb
+++ db/migrate/20171130181123_create_skills.rb
@@ -1,7 +1,7 @@
 class CreateSkills < ActiveRecord::Migration[5.1]
   def change
     create_table :skills do |t|
-      t.string :tile
+      t.string :title
       t.integer :percent_utilized
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20171130181123) do
   end
 
   create_table "skills", force: :cascade do |t|
-    t.string "tile"
+    t.string "title"
     t.integer "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
