e7031d427b868377426729d7877296efc8ae4ef0
Casey Lund <caseylund@Caseys-Air.ad.uvu.edu>
2017-12-01 11:29:32 -0700

    Created seeds file for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "My Great Service",
+    body: "Proin nec nunc velit. Donec et malesuada arcu. Praesent venenatis dolor sit amet orci fringilla pulvinar. Vivamus a diam sed leo vulputate sollicitudin in quis quam. Aenean purus tellus, hendrerit id consequat ut, dignissim sit amet ex. Aliquam sagittis bibendum nulla, ac mollis felis blandit ac. Proin luctus vitae sapien nec condimentum. Integer ut dui mattis, volutpat justo id, lobortis sem. Nulla enim lorem, mattis in laoreet vitae, imperdiet non ligula.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200",
+    )
+end
