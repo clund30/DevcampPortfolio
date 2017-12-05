7bac5b4bce18f7ae37811f70c926a1628f875ddc
Casey Lund <caseylund@Caseys-Air.ad.uvu.edu>
2017-12-05 14:58:33 -0700

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,3 +43,13 @@ end
     thumb_image: "http://via.placeholder.com/350x200",
     )
 end
+
+puts "9 portfolio items created"
+
+3.times do |technology|
+  Portfolio.last.technologies.create!(
+    name: "Technology #{technology}"
+  )
+end
+
+puts "3 technologies created"
